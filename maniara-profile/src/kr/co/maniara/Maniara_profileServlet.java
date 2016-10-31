fabe0c8d505524a9c9c6524b7fda4050aa52687d
Deokyoon Ko <Deokyoon Ko@DeokyoonKo>
2016-10-31 16:26:30 +0900

    initial

---
+++ maniara-profile/src/kr/co/maniara/Maniara_profileServlet.java
@@ -0,0 +1,12 @@
+package kr.co.maniara;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Maniara_profileServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
